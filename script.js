147827cc438796ecdfc71cb8c1be5b445772825d
mathis-serra <mathis.serra@laplateforme.io>
2024-04-23 15:24:18 +0200

    [MERGED] Both code kevin and me

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 var scoreCount = document.getElementById('score-count');
 var clickButton = document.getElementById('click');
 var resetButton = document.getElementById('reset-button');
-var lastscoreElement = document.getElementById('lastscore');
+const lastscoreElement = document.getElementById('lastscore');
+const generateFlamme = document.getElementById('textflammepersecond');
 var score = localStorage.getItem('score') ? parseInt(localStorage.getItem('score')) : 0;
-var flamesPerSecond = 1; 
+var flamesPerSecond = 1;
 
-
+// Mise à jour de l'affichage du score
 function updateScore() {
     scoreCount.textContent = score;
 }
 
-
+// Ajout du score
 function addScore() {
     score++;
     updateScore();
     localStorage.setItem('score', score); 
 }
+
 
 function resetScore() {
     
@@ -31,17 +33,10 @@ function resetScore() {
         return;
     }
 }
-
-
-
+// Fonction pour ajouter des flammes par seconde
 function addFlamesPerSecond() {
     score += flamesPerSecond;
     updateScore();
-}
-
-
-function displayLastScore() {
-    lastscoreElement.textContent = "LAST SCORE : " + score;
 }
 
 clickButton.addEventListener('click', addScore);
@@ -49,7 +44,11 @@ resetButton.addEventListener('click', resetScore);
 
 updateScore();
 
-
+// Démarre les flammes par seconde
 setInterval(addFlamesPerSecond, 1000);
 
-displayLastScore();
+// Affichage du dernier score
+lastscoreElement.textContent = "LAST SCORE : " + score;
+
+// Afficher la génération des flammes par seconde
+generateFlamme.textContent = "You generate " + flamesPerSecond + " flames per second";
