a6b378380ed12e7602b819798b1665475af5104e
mathis-serra <mathis.serra@laplateforme.io>
2024-04-24 16:13:00 +0200

    [CHANGE] FONT , FLAME.GIF, [ADD] new upgrades

--- script.js
+++ script.js
@@ -6,25 +6,24 @@ var clickUpgrade2 = document.getElementById('upgrade2');
 var clickUpgrade3 = document.getElementById('upgrade3');
 var clickUpgrade4 = document.getElementById('upgrade4');
 var clickUpgrade5 = document.getElementById('upgrade5');
-const lastscoreElement = document.getElementById('lastscore');
-const generateFlamme = document.getElementById('textflammepersecond');
+var lastscoreElement = document.getElementById('lastscore');
+var generateFlamme = document.getElementById('textflammepersecond');
 var score = localStorage.getItem('score') ? parseInt(localStorage.getItem('score')) : 0;
 var flamesPerSecond = localStorage.getItem('flamesPerSecond') ? parseInt(localStorage.getItem('flamesPerSecond')) : 0;
 var upgrade1Price = localStorage.getItem('upgrade1Price') ? parseInt(localStorage.getItem('upgrade1Price')) : 10;
 var upgrade2Price = localStorage.getItem('upgrade2Price') ? parseInt(localStorage.getItem('upgrade2Price')) : 100;
-
-
-
-
-
-
-
+var upgrade3Price = localStorage.getItem('upgrade3Price') ? parseInt(localStorage.getItem('upgrade3Price')) : 1000;
+var upgrade4Price = localStorage.getItem('upgrade4Price') ? parseInt(localStorage.getItem('upgrade4Price')) : 10000;
+var upgrade5Price = localStorage.getItem('upgrade5Price') ? parseInt(localStorage.getItem('upgrade5Price')) : 100000;
 
 function saveGameState() {
     localStorage.setItem('score', score);
     localStorage.setItem('flamesPerSecond', flamesPerSecond);
     localStorage.setItem('upgrade1Price', upgrade1Price);
     localStorage.setItem('upgrade2Price', upgrade2Price);
+    localStorage.setItem('upgrade3Price', upgrade3Price);
+    localStorage.setItem('upgrade4Price', upgrade4Price);
+    localStorage.setItem('upgrade5Price', upgrade5Price);
 }
 
 function loadGameState() {
@@ -32,14 +31,22 @@ function loadGameState() {
     flamesPerSecond = localStorage.getItem('flamesPerSecond') ? parseInt(localStorage.getItem('flamesPerSecond')) : 0;
     upgrade1Price = localStorage.getItem('upgrade1Price') ? parseInt(localStorage.getItem('upgrade1Price')) : 10;
     upgrade2Price = localStorage.getItem('upgrade2Price') ? parseInt(localStorage.getItem('upgrade2Price')) : 100;
+    upgrade3Price = localStorage.getItem('upgrade3Price') ? parseInt(localStorage.getItem('upgrade3Price')) : 1000;
+    upgrade4Price = localStorage.getItem('upgrade4Price') ? parseInt(localStorage.getItem('upgrade4Price')) : 10000;
+    upgrade5Price = localStorage.getItem('upgrade5Price') ? parseInt(localStorage.getItem('upgrade5Price')) : 100000;
+    
+    updateScore();
+    updateUpgrade1Price();
+    updateUpgrade2Price();
+    updateUpgrade3Price();
+    updateUpgrade4Price();
+    updateUpgrade5Price();
+    updateFlamesPerSecondText();
 }
-
-
 
 function updateScore() {
     scoreCount.textContent = score;
 }
-
 
 function addScore() {
     score++;
@@ -47,7 +54,6 @@ function addScore() {
     localStorage.setItem('score', score);
     saveGameState();
 }
-
 
 function updateFlamesPerSecondText() {
     generateFlamme.textContent = "You generate " + flamesPerSecond + " flames per second";
@@ -58,96 +64,137 @@ function updateUpgrade1Price() {
     var upgrade1PriceElement = document.getElementById('upgrade1-price');
     upgrade1PriceElement.textContent = upgrade1Price + " kW/m²";
 }
+
 function updateUpgrade2Price() {
     var upgrade2PriceElement = document.getElementById('upgrade2-price');
     upgrade2PriceElement.textContent = upgrade2Price + " kW/m²";
 }
 
-function upgrade1(){
+function updateUpgrade3Price() {
+    var upgrade3PriceElement = document.getElementById('upgrade3-price');
+    upgrade3PriceElement.textContent = upgrade3Price + " kW/m²";
+}
+
+function updateUpgrade4Price() {
+    var upgrade4PriceElement = document.getElementById('upgrade4-price');
+    upgrade4PriceElement.textContent = upgrade4Price + " kW/m²";
+}
+
+function updateUpgrade5Price() {
+    var upgrade5PriceElement = document.getElementById('upgrade5-price');
+    upgrade5PriceElement.textContent = upgrade5Price + " kW/m²";
+}
+
+function upgrade1() {
     if (score >= upgrade1Price) {
         flamesPerSecond++;
-        score -= upgrade1Price; 
-        upgrade1Price *= 2 * 2; 
-        updateScore();
-        updateFlamesPerSecondText(); 
-        updateUpgrade1Price(); 
-        localStorage.setItem('score', score);
-        saveGameState(); 
-        return flamesPerSecond;
-    } else {
-        alert("You don't have enough score to buy this upgrade!");
-    }
-}
-
-function upgrade2(){
-    if (score >= upgrade2Price) {
-        flamesPerSecond+= 2;
-        score -= upgrade2Price; 
-        upgrade2Price *= 2 * 2; 
+        score -= upgrade1Price;
+        upgrade1Price *= 2 * 2;
         updateScore();
         updateFlamesPerSecondText();
-        updateUpgrade2Price(); 
+        updateUpgrade1Price();
         localStorage.setItem('score', score);
-        saveGameState(); 
+        saveGameState();
         return flamesPerSecond;
     } else {
         alert("You don't have enough score to buy this upgrade!");
     }
 }
-function upgrade3(){
-    flamesPerSecond+= 10;
-    updateScore();
-    updateFlamesPerSecondText(); 
-    localStorage.setItem('score', score);
-    return flamesPerSecond;
-}
-function upgrade4(){
-    flamesPerSecond+= 20;
-    updateScore();
-    updateFlamesPerSecondText(); 
-    localStorage.setItem('score', score);
-    return flamesPerSecond;
-}
-function upgrade5(){
-    flamesPerSecond+= 100;
-    updateScore();
-    updateFlamesPerSecondText(); 
-    localStorage.setItem('score', score);
-    return flamesPerSecond;
+
+function upgrade2() {
+    if (score >= upgrade2Price) {
+        flamesPerSecond += 2;
+        score -= upgrade2Price;
+        upgrade2Price *= 2 * 2;
+        updateScore();
+        updateFlamesPerSecondText();
+        updateUpgrade2Price();
+        localStorage.setItem('score', score);
+        saveGameState();
+        return flamesPerSecond;
+    } else {
+        alert("You don't have enough score to buy this upgrade!");
+    }
 }
 
+function upgrade3() {
+    if (score >= upgrade3Price) {
+        flamesPerSecond += 10;
+        score -= upgrade3Price;
+        upgrade3Price *= 2 * 2;
+        updateScore();
+        updateFlamesPerSecondText();
+        updateUpgrade3Price();
+        localStorage.setItem('score', score);
+        saveGameState();
+        return flamesPerSecond;
+    } else {
+        alert("You don't have enough score to buy this upgrade!");
+    }
+}
 
+function upgrade4() {
+    if (score >= upgrade4Price) {
+        flamesPerSecond += 50;
+        score -= upgrade4Price;
+        upgrade4Price *= 2 * 2;
+        updateScore();
+        updateFlamesPerSecondText();
+        updateUpgrade4Price();
+        localStorage.setItem('score', score);
+        saveGameState();
+        return flamesPerSecond;
+    } else {
+        alert("You don't have enough score to buy this upgrade!");
+    }
+}
 
-
+function upgrade5() {
+    if (score >= upgrade5Price) {
+        flamesPerSecond += 100;
+        score -= upgrade5Price;
+        upgrade5Price *= 2 * 2;
+        updateScore();
+        updateFlamesPerSecondText();
+        updateUpgrade5Price();
+        localStorage.setItem('score', score);
+        saveGameState();
+        return flamesPerSecond;
+    } else {
+        alert("You don't have enough score to buy this upgrade!");
+    }
+}
 
 function resetScore() {
     var confirmation = confirm("Are you sure you want to reset?");
     if (confirmation) {
         score = 0;
         updateScore();
-        flamesPerSecond = 0; 
+        flamesPerSecond = 0;
         updateFlamesPerSecondText();
         upgrade1Price = 10;
         updateUpgrade1Price();
         upgrade2Price = 100;
         updateUpgrade2Price();
+        upgrade3Price = 1000;
+        updateUpgrade3Price();
+        upgrade4Price = 10000;
+        updateUpgrade4Price();
+        upgrade5Price = 100000;
+        updateUpgrade5Price();
         localStorage.setItem('score', score);
-        saveGameState(); 
+        saveGameState();
     } else {
         return;
     }
 }
 
-
-
-
 function addFlamesPerSecond() {
     score += flamesPerSecond;
     updateScore();
-    saveGameState(); 
+    updateFlamesPerSecondText();
+    saveGameState();
 }
-
-
 
 clickButton.addEventListener('click', addScore);
 resetButton.addEventListener('click', resetScore);
@@ -163,9 +210,11 @@ setInterval(addFlamesPerSecond, 1000);
 
 lastscoreElement.textContent = "LAST SCORE : " + score;
 
-
 updateUpgrade1Price();
 updateUpgrade2Price();
+updateUpgrade3Price();
+updateUpgrade4Price();
+updateUpgrade5Price();
 updateFlamesPerSecondText();
 
 window.addEventListener('beforeunload', function() {
@@ -174,8 +223,5 @@ window.addEventListener('beforeunload', function() {
 
 window.addEventListener('load', function() {
     loadGameState();
-    updateScore();
-    updateFlamesPerSecondText();
-    updateUpgrade1Price();
-    updateUpgrade2Price();
-}  );
+    
+});
